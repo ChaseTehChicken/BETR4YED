42a70c66e1bf0cc3c348aa1d8073138a97fd8cbd
Chaseyy <38933237+ChaseTehChicken@users.noreply.github.com>
2020-07-29 14:41:18 +1000

    UwU
    removed pointless header

--- cogs/fun.py
+++ cogs/fun.py
@@ -253,10 +253,6 @@ class fun(commands.Cog):
     async def uwuify(self, ctx, *, args):
         flags = uwuify.SMILEY | uwuify.YU
         await ctx.send(uwuify.uwu(args, flags=flags))
-    
-    @commands.command()
-    async def # finishing soon
-
-
+                           
 def setup(client):
     client.add_cog(fun(client))
